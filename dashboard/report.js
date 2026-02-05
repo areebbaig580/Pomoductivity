c72bc0f5fd30423e4da64aa9fcd79df4131f7f52
areebbaig580 <areebbaig580@gmail.com>
2026-02-05 23:49:05 +0530

    Updated sessions update function

--- dashboard/report.js
+++ dashboard/report.js
@@ -1,6 +1,65 @@
 const ctx = document.getElementById('myChart');
 const ctx2 = document.getElementById('myChart2');
+const noSession = document.getElementById('session');
 
+function getSession() {
+
+    let session = localStorage.getItem('pomodoroSessions');
+    return session ? JSON.parse(session) : [];
+
+};
+
+
+function getTodayDate() {
+    return new Date().toISOString().split('T')[0];
+};
+
+
+function getTodaySession() {
+    let sessions = getSession();
+    let today = getTodayDate();
+
+    return sessions.filter(s => s.date === today && s.type === "focus").length;
+
+};
+
+function getWeeklySessions() {
+    let allsessions = getSession();
+    let focusSessions = allsessions.filter(s => s.type === "focus");
+
+    let today = new Date();
+    let dayOfWeek = today.getDay();
+    let daysFromMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+
+    let monday = new Date(today);
+    monday.setDate(today.getDate() - daysFromMonday);
+
+
+    let dailyCounts = [0, 0, 0, 0, 0, 0, 0];
+
+    for (let i = 0; i < 7; i++) {
+        let currentDay = new Date(monday);
+        currentDay.setDate(monday.getDate() + i);
+        let dateString = currentDay.toISOString().split('T')[0];
+
+        let count = focusSessions.filter(s => s.date === dateString).length;
+        dailyCounts[i] = count;
+    }
+
+
+    return dailyCounts;
+
+
+};
+
+
+function updateChart() {
+    noSession.innerHTML = getTodaySession();
+};
+
+
+updateChart();
+let daysData = getWeeklySessions();
 
 new Chart(ctx, {
     type: 'bar',
@@ -34,8 +93,8 @@ new Chart(ctx2, {
     data: {
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', `Sat`, `Sun`],
         datasets: [{
-            label: 'Toatal Sessions',
-            data: [],
+            label: 'Total Sessions',
+            data: daysData,
             backgroundColor: '#8A75E6'
         }]
     },
